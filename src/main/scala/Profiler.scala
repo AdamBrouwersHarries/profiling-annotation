115d0dae79dbf5409e2a16c6a3a8c436dbc015a1
Adam Harries <adam.harries@ed.ac.uk>
2017-06-06 18:08:08 +0100

    try to fix compile error 2

--- src/main/scala/Profiler.scala
+++ src/main/scala/Profiler.scala
@@ -24,7 +24,7 @@ object Profile {
             val start = System.nanoTime()
             val profSpliceResultValueNoConflict = {..$body}
             val end = System.nanoTime()
-            println(ctx)
+            println(${ctx})
             println("PROFILING_DATUM: (\""+${methodName.toString}+"\", " + (end-start) + ")")
             profSpliceResultValueNoConflict
           }"""
