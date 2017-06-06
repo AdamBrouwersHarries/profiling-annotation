67dd1726b6800c119fdb4aa1b4ea760ea3d03573
Adam Harries <adam.harries@ed.ac.uk>
2017-06-06 13:21:22 +0100

    add code and gitignore

---
+++ build.sbt
@@ -0,0 +1,27 @@
+import Dependencies._
+
+name := "profiling-annotation"
+
+version := "0.0.1"
+
+// scalaVersion := "2.11.8"
+
+// lazy val root = (project in file(".")).
+//   settings(
+//     inThisBuild(List(
+//       organization := "com.example",
+//       scalaVersion := "2.12.1",
+//       version      := "0.1.0-SNAPSHOT"
+//     )),
+//     name := "Hello",
+//     libraryDependencies += scalaTest % Test
+//   )
+
+libraryDependencies ++= Seq(
+  "org.scala-lang" % "scala-compiler" % "2.11.8"
+)
+
+val paradiseVersion = "2.1.0-M5"
+
+addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
+
---
+++ src/main/profiler/Profiler.scala
@@ -0,0 +1,37 @@
+import scala.annotation.StaticAnnotation
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
+
+class Profile extends StaticAnnotation {
+  def macroTransform(annottees: Any*) = macro Profile.impl
+}
+
+object Profile {
+  def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    import c.universe._
+
+    val result = {
+      annottees.map(_.tree).toList match {
+        case q"$mods def $methodName[..$tpes](...$args): $returnType = { ..$body }" :: Nil => {
+          q"""$mods def $methodName[..$tpes](...$args): $returnType =  {
+            val start = System.nanoTime()
+            val result = {..$body}
+            val end = System.nanoTime()
+            println(${methodName.toString} + " elapsed time: " + (end - start) + "ns")
+            result
+          }"""
+        }
+        case _ => c.abort(c.enclosingPosition, "Annotation @Profile can be used only with methods")
+      }
+    }
+    c.Expr[Any](result)
+  }
+
+  def profile[T](name: String,f: () => T) : T = {
+    val start = System.nanoTime()
+    val r: T = f()
+    val end = System.nanoTime()
+    println(${methodName.toString} + " elapsed time: " + (end - start) + "ns")
+    r
+  }
+}
